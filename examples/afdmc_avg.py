5f33dc496b80db6e9fb9338ca7edf7b7b8bef029
jfox <jfox@anl.gov>
2024-04-19 16:24:45 -0500

    need to fix mixing

--- examples/afdmc_avg.py
+++ examples/afdmc_avg.py
@@ -1,11 +1,9 @@
 from quap import *
-from quap.exact import ExactGFMC
 
 n_particles = 2
 dt = 0.001
 n_samples = 10000
-seed = 0
-
+seed = 11
 
 def main():
     ket = AFDMCSpinIsospinState(n_particles,'ket', read_from_file("./data/h2/fort.770",complex=True, shape=(2,4,1)))
@@ -18,31 +16,33 @@ def main():
     pot.read_coulomb("./data/h2/fort.7704")
     pot.read_spinorbit("./data/h2/fort.7705")
 
-    # prop = AFDMCPropagatorHS(n_particles, dt, include_prefactor=True, mix=True, seed=seed)
-    prop = AFDMCPropagatorRBM(n_particles, dt, include_prefactor=True, mix=True, seed=seed)
+    prop = AFDMCPropagatorHS(n_particles, dt, include_prefactor=True, mix=True, seed=seed)
+    # prop = AFDMCPropagatorRBM(n_particles, dt, include_prefactor=True, mix=True, seed=seed)
     
     integ = Integrator(pot, prop)
-    integ.controls["sigma"] = True
-    integ.controls["sigmatau"] = False
-    integ.controls["tau"] = False
-    integ.controls["coulomb"] = False
-    integ.controls["spinorbit"] = False
-    integ.controls["balanced"] = True 
-    integ.setup(n_samples=n_samples, seed=seed)
+    integ.setup(n_samples=n_samples,
+                balance=True,
+                mix=True,
+                seed=seed,
+                sigma=True,
+                sigmatau=True,
+                tau=True,
+                coulomb=False,
+                spinorbit=False)
     
     b_array = integ.run(bra, ket, parallel=True)
     b_m = np.mean(b_array)
     b_s = np.std(b_array)/np.sqrt(n_samples)
-    print(f'bracket = {b_m} +/- {b_s}')
-    # chistogram(b_array, filename='hs_test.pdf', title='HS test')
-    
-    ex = ExactGFMC(n_particles)
-    g_exact = ex.make_g_exact(n_particles, dt, pot, integ.controls)
-    b_exact = bra * g_exact * ket
-    print('exact = ',b_exact)
+    b_exact = integ.exact(bra.to_manybody_basis(), ket.to_manybody_basis())
 
+    print("<bra|ket> = ", bra * ket)
+    print(f'<bra|G|ket> = {b_m} +/- {b_s}')
+    print('exact = ',b_exact)
     print("ratio = ", np.abs(b_m)/np.abs(b_exact) )
     print("abs error = ", abs(1-np.abs(b_m)/np.abs(b_exact)) )
+
+   # chistogram(b_array, filename='hs_test.pdf', title='HS test')
+
 
 if __name__ == "__main__":
     main()
